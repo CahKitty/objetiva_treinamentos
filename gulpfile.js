8efdd6fb81c4046898f7e7012307a07d57795194
Carla <carla.annunciato@eduzz.com>
2019-02-26 13:12:16 -0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,84 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    package = require('./package.json');
+    plumber = require('gulp-plumber');
+    concat = require('gulp-concat');
 
-var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
-];
 
-function sass() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
-    ]))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
-};
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
 
-function serve() {
-  browserSync.init({
-    server: "./"
-  });
+gulp.task('css', function () {
+    return gulp.src('src/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(sass({
+        errLogToConsole: true,
+        includePaths: [ 'src/styles/scss/' ],
+    }))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(cssnano())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
 
-  gulp.watch("scss/*.scss", sass);
-  gulp.watch("*.html").on('change', browserSync.reload);
-}
+gulp.task('js',function(){
+  gulp.src('src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(concat('scripts.js'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('app/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
 
-gulp.task('sass', sass);
-gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "app"
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+// gulp.task("default", function () {
+//     gulp.start("serve");
+//   });
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sync', function() {
+ browserSync.init({
+ proxy: "my_project.dev",
+ files: "*.css,*.php,css/*css"
+});
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("src/scss/**", ['css']);
+    gulp.watch("src/js/*.js", ['js']);
+    gulp.watch("app/*.html", ['bs-reload']);
+});
